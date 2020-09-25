067176a731e9965148e8ca9678847f0bbdd661a8
Lucas Dupree <lucasdupree24@gmail.com>
2020-09-24 20:38:26 -0700

    test

--- weather.py
+++ weather.py
@@ -1,13 +1,16 @@
 import requests
-import pprint
-import re
+import pprint # pretty print module for json data
+#import re # regex module
 
 
 def data_collect():
+    # while loop that allows program to run until user enters "quit" command
     while True:
         zip_code = str(input("Please enter a zip code: "))
+        # get request for api w/ user input variable to define zip code parameter
         api_results = requests.get('https://api.openweathermap.org/data/2.5/weather?zip=' + str(zip_code) + ','
-                                   'us&appid=24658256b62af67c116c8df0435b7782')
+                                   'us&appid=24658256b62af67c116c8df0435b7782', timeout=0.001) # requests library connection
+                                                                                               # timeout
         pprint.pprint(api_results.json())
         if zip_code == "Quit" or zip_code == "quit":
             break
@@ -16,9 +19,11 @@ def data_collect():
 
 
 def data_validator(zip_code):
+    # Attempted implementation of regex to check for valid zip code
     # zip_code_pattern = '^[0-9]{5}(?:-[0-9]{4})?$'
     empty_input = ''
     # pattern_match = re.match(zip_code_pattern, str(zip_code))
+    # control flow to check for empty user input. Still working on this part
     if len(zip_code) < 0:
         print("empty")
     else:
@@ -26,3 +31,6 @@ def data_validator(zip_code):
 
 
 data_collect()
+
+
+
